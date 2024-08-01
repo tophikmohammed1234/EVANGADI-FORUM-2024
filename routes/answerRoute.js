e96cae71f6737a784c9336399f343f32aeed4e31
yabiball <158844492+yabiball@users.noreply.github.com>
2024-08-01 23:30:26 +0300

    merge

--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -1,13 +1,9 @@
 const express = require("express");
 
-const router = express.Router();
-
-const { answer } = require("../controller/answerController");
-
 // answer route
 router.post("/", answer);
 const router = express.Router();
-const { getAnswer } = require("../controller/answerController");
+const { getAnswer,answer } = require("../controller/answerController");
 router.get("/:questionid", getAnswer);
 
 
