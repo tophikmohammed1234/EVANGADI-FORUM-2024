843ae8aa3f7ef14771f8689ed2b8cc997313724e
Teslule <luletesfaye2020@gmail.com>
2024-08-01 15:02:27 -0400

    authMiddleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+
+async function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  // check if the authorization header starts with Bearer & extracts the token
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ msg: "Authentication invalid" });
+  }
+  // Extract the token from the header
+  const token = authHeader.split("")[1];
+
+  try {
+    // verify the token using the secret key
+    const { username, userid } = jwt.verify(token, process.env.JWT_SECRET);
+    //   attach user data to the request object
+    req.user = { username, userid };
+    //   Pass control to the next middleware -questions and answers?
+    next();
+  } catch (error) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ msg: "Authentication invalid" });
+  }
+}
+
+module.exports = authMiddleware;
