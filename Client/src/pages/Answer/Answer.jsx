6c0d4b12dee6929fd7e1233940cc96dace0734a3
Tophik Mohammed <117739281+tophikmohammed1234@users.noreply.github.com>
2024-08-08 15:47:48 +0300

    final commit

--- Client/src/pages/Answer/Answer.jsx
+++ Client/src/pages/Answer/Answer.jsx
@@ -17,6 +17,7 @@ const Answer = () => {
 	const [isanswered, setAnswer] = useState(false);
 	const [charCount, setCharCount] = useState(0);
 	const [showCharCount, setShowCharCount] = useState(false);
+	const [answerRequired, setanswerRequired] = useState(false);
 
 	// Fetch the question title and description
 	async function getQuestionDetail() {
@@ -31,9 +32,8 @@ const Answer = () => {
 			};
 			const res = await axiosInstance.get(`/questions/${question_id}`, config);
 
-			// Accessing the first item in the question array
-			const question = res.data.question && res.data.question[0];
-			setQuestionDetail(question || {});
+			const question = res.data.question[0];
+			setQuestionDetail(question);
 		} catch (error) {
 			console.log(error);
 		}
@@ -79,21 +79,26 @@ const Answer = () => {
 			};
 			const res = await axiosInstance.get(`/answers/${question_id}`, config);
 
-			// Accessing the answer array
-			setAnswers(res.data.answers || []);
+			setAnswers(res.data.answers);
 		} catch (error) {
 			console.log(error.response);
 		}
 	}
 
 	// Post an answer
-	async function answerForQuestion(e) {
+	async function postAnswer(e) {
 		e.preventDefault();
 
-		const Answer = postedanswer.current?.value;
+		const Answer = postedanswer.current.value;
 
+		let hasError = false;
 		if (!Answer) {
-			alert("Please provide the answer");
+			setanswerRequired(true);
+			hasError = true;
+		} else {
+			setanswerRequired(false);
+		}
+		if (hasError) {
 			return;
 		}
 
@@ -120,7 +125,7 @@ const Answer = () => {
 			getAnswers();
 			setAnswer(true);
 
-			// Set a timer to remove the success message after 3 seconds
+			// Set a timer to remove the Answer posted successfully  message after 3 seconds
 			const timeoutId = setTimeout(() => {
 				setAnswer(false);
 			}, 3000);
@@ -136,9 +141,8 @@ const Answer = () => {
 		getAnswers();
 	}, [question_id]);
 
-	// Destructure question_detail with default values
-	const { title = "No Title", description = "No Description" } =
-		question_detail;
+	// Destructure question_detail values
+	const { title, description } = question_detail;
 
 	return (
 		<LayOut>
@@ -179,14 +183,14 @@ const Answer = () => {
 								<AnswerCard key={i} getAnswer={getAnswer} />
 							))
 						) : (
-							<p>No answers yet.</p> // Display a message if there are no answers
+							<p>No answers yet.</p>
 						)}
 					</section>
 				</section>
 
 				{/* ============POST ANSWER SECTION============== */}
 				<section className={`${classes.post_answer_wrapper}`}>
-					<form onSubmit={answerForQuestion}>
+					<form onSubmit={postAnswer}>
 						{isanswered && (
 							<h5 className={classes.alert_msg}>Answer posted successfully</h5>
 						)}
@@ -200,8 +204,13 @@ const Answer = () => {
 							name="postedanswer"
 							id="postedanswer"
 							placeholder="Your answer ..."
-							className="w-100 mt-4 p-3"
+							className={`${
+								answerRequired ? classes.answer_filed_error : ""
+							}  w-100 mt-4 p-3`}
 						></textarea>
+						{answerRequired && (
+							<p className={`text-danger `}>Answer is required</p>
+						)}
 						{showCharCount && (
 							<small className={`${classes.characterCount} my-3`}>
 								{charCount}/200
--- Client/src/pages/Answer/AnswerCard.jsx
+++ Client/src/pages/Answer/AnswerCard.jsx
@@ -3,21 +3,21 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import classes from "./Answer.module.css";
 
 const AnswerCard = ({ getAnswer }) => {
-  const { username, answer } = getAnswer;
-  return (
-    <>
-      <div className={`${classes.answerWrapper} container`}>
-        <div className={classes.userProfile}>
-          <div>
-            <AccountCircleIcon />
-          </div>
-          <div className={classes.username}>{username}</div>
-        </div>
+	const { username, answer } = getAnswer;
+	return (
+		<>
+			<div className={`${classes.answerWrapper} container`}>
+				<div className={classes.userProfile}>
+					<div>
+						<AccountCircleIcon />
+					</div>
+					<div className={classes.username}>{username}</div>
+				</div>
 
-        <div className={classes.answers}>{answer}</div>
-      </div>
-    </>
-  );
+				<div className={classes.answers}>{answer}</div>
+			</div>
+		</>
+	);
 };
 
 export default AnswerCard;
